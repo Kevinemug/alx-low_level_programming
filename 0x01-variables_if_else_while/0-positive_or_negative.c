f4ef75439e83d9186a8a441cb85150b9fc986f8e
Kevinemug <kevineeuwamahoro@gmail.com>
2022-06-03 17:33:23 -0700

    program will assign a random number to the variable n each time it is executed.

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,24 +2,32 @@
 #include<stdlib.h>
 #include<time.h>
 
-/** This program will assign a random number to a variable n each time it is executed.
+/*
+ *
+ * * This program will assign a random number
+ * to a variable n each time it is executed.
  *
  */
-
 int main(void)
 {
-	int n;
-	srand(time(0));
-	n=rand()-RAND_MAX/2;
-	
-	if(n>0)
-		printf("%d is positive \n",n);
-	else if(n<0)
-		printf("%d is negative \n",n);
-	else
-		printf("%d is zero \n",n);
-	return (0);
+int n;
 
-}	
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n > 0)
+{
+printf("%d is positive \n", n);
+}
+else if (n == 0)
+{
+printf("%d is zero \n", n);
+}
+else
+{
+printf("%d is negative \n", n);
+}
+
+return (0);
+}
 
 
---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,16 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<time.h>
+/**
+ *
+ *  program will assign a random number to the variable n each time it is executed
+ */
+int main(void)
+{
+	int n;
+	srand(time(0));
+	n= rand() - RAND_MAX/2
+		if(n % 2 > 5)
+			printf("last
+
+	 
