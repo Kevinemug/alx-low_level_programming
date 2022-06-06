26d2e3d6b8d4057a9aa3b4f21fa8167cb5adc94f
Kevinemug <kevineeuwamahoro@gmail.com>
2022-06-06 14:02:50 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers of base 16 in lower cases
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num;
+	char letter;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+
+	for (letter = 'a'; letter <= 'f'; letter++)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+}
