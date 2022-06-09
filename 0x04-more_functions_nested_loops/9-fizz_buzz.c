82f62d0204f8f42ce5c735524a5a14b1a1af0f90
Kevinemug <kevineeuwamahoro@gmail.com>
2022-06-09 09:56:26 -0700

    Hot interview programming question!

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *  main - prints the numbers from 1 to 100, followed by a new line
+ *  prints bizz and buzz for specific numbers
+ *  Return: Always 0,
+ */
+
+int main(void)
+{
+int i;
+for (i = 1; i <= 100; i++)
+{
+if (i % 3 == 0 && i % 5 != 0)
+{
+printf(" Fizz");
+}
+else if (i % 5 == 0 && i % 3 != 0)
+{
+printf(" Buzz");
+}
+else if (i == 1)
+{
+printf("%d", i);
+}
+else
+{
+printf(" %d", i);
+}
+}
+printf("\n");
+return (0);
+}
