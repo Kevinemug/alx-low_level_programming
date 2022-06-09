c91e7e25abeb448a5e2ea6c982b9bc79fc5ebb9c
Kevinemug <kevineeuwamahoro@gmail.com>
2022-06-09 07:13:29 -0700

    deeping dive in c programming

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase character.
+ * @c : character to check the case
+ * Return: 0 or 1.
+ */
+int _isupper(int c)
+{
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
