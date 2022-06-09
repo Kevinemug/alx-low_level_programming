3123634e9f0cabe4380beafa76691b78ca91e814
Kevinemug <kevineeuwamahoro@gmail.com>
2022-06-08 19:10:47 -0700

    committing

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - causes an infinite loop
+ * Return: is 0
+ *
+ */
+
+int main(void)
+{
+int i;
+printf("Infinite loop incoming :(\n");
+i = 0;
+/*
+ * while (i < 10)
+ * {
+ * putchar(i);
+ * }
+ */
+printf("Infinite loop avoided! \\o/\n");
+return (0);
+}
