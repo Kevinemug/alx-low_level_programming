a1f6f1c545869b9af0d90825ad258789a873184d
Kevinemug <kevineeuwamahoro@gmail.com>
2022-06-09 11:00:50 -0700

    today's task

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -8,7 +8,10 @@
  *  @year: year
  *  Return: void
  */
+
 void print_remaining_days(int month, int day, int year)
+{
+if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
 {
 if (month >= 3 && day >= 60)
 {
@@ -30,4 +33,5 @@ printf("Remaining days: %d\n", 365 - day);
 }
 }
 }
+
 
