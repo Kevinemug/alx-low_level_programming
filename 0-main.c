045bc694de91bd5608682bb19980db47904eb109
Kevinemug <kevineeuwamahoro@gmail.com>
2022-06-08 18:57:39 -0700

    today's tasks

---
+++ 0-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - tests funtion positve or negative
+ *
+ * Return: Always 0,
+ */
+
+int main(void)
+{
+int i;
+i = 0;
+positive_or_negative(i);
+return (0);
+}
---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - test the positive or negative function
+ *
+ * Return: Always 0,
+ */
+
+int main(void)
+{
+int i;
+i = 0;
+positive_or_negative(i);
+return (0);
+}
