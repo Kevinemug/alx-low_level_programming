3038795eb22855e7a17e34a19ff36dc22b95a319
Kevinemug <kevineeuwamahoro@gmail.com>
2022-06-13 13:25:21 -0700

    fixed a bug

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- i* reset_to_98 - takes a pointer to an int as parameter
+ * reset_to_98 - takes a pointer to an int as parameter
  * and updates the value it points to to 98.
  * @n: pointer to integer
  * Return: 0 Success
