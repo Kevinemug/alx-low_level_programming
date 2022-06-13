0de373ec53ce4364e513ded30c7d159d79fd7169
Kevinemug <kevineeuwamahoro@gmail.com>
2022-06-13 13:14:09 -0700

    task

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,7 +3,7 @@ void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
-void rev_string(char *s
+void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
 void _putchar(char);
